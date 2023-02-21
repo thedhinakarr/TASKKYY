85fbec2168646d353530488067832be07f129c7f
Dinakar <dvk@Dinakars-MacBook-Pro.local>
2023-02-21 15:59:05 +0530

    fTasky

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 import express  from "express";
 import config from "config";
 import dbConnect from "./dbConnect.js";
+import path from "path";
+import { fileURLToPath } from "url"
+
 
 import UserRouter from "./controllers/users/index.js";
 import TaskRouter from "./controllers/tasks/index.js";
@@ -9,10 +12,19 @@ const port = config.get("PORT");
 const app = express();
 
 
+const __filename = fileURLToPath(import.meta.url); //
+const __dirname = path.dirname(__filename); //
+
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.use(express.json());
+
 app.use("/api/user",UserRouter);
 app.use("/api/task",TaskRouter);
 
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
 
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`)
